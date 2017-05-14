077b3efa4fd4385992f51476038b599575bc49d8
xzhang1234 <finish615@gmail.com>
2017-05-14 13:21:47 -0500

    Create subsets.java

---
+++ Permutation/subsets.java
@@ -0,0 +1,55 @@
+/**
+ * 本代码由九章算法编辑提供。版权所有，转发请注明出处。
+ * - 九章算法致力于帮助更多中国人找到好的工作，教师团队均来自硅谷和国内的一线大公司在职工程师。
+ * - 现有的面试培训课程包括：九章算法班，系统设计班，算法强化班，Java入门与基础算法班，Android 项目实战班，Big Data 项目实战班，
+ * - 更多详情请见官方网站：http://www.jiuzhang.com/?source=code
+ */
+
+// 递归：实现方式，一种实现DFS算法的一种方式
+class Solution {
+    /**
+     * @param S: A set of numbers.
+     * @return: A list of lists. All valid subsets.
+     */
+    public ArrayList<ArrayList<Integer>> subsets(int[] nums) {
+        ArrayList<ArrayList<Integer>> results = new ArrayList<>();
+        
+        if (nums == null) {
+            return results;
+        }
+        
+        if (nums.length == 0) {
+            results.add(new ArrayList<Integer>());
+            return results;
+        }
+        
+        Arrays.sort(nums);
+        helper(new ArrayList<Integer>(), nums, 0, results);
+        return results;
+    }
+    
+    
+    // 递归三要素
+    // 1. 递归的定义：在 Nums 中找到所有以 subset 开头的的集合，并放到 results
+    private void helper(ArrayList<Integer> subset,
+                        int[] nums,
+                        int startIndex,
+                        ArrayList<ArrayList<Integer>> results) {
+        // 2. 递归的拆解
+        // deep copy
+        // results.add(subset);
+        results.add(new ArrayList<Integer>(subset));
+        
+        for (int i = startIndex; i < nums.length; i++) {
+            // [1] -> [1,2]
+            subset.add(nums[i]);
+            // 寻找所有以 [1,2] 开头的集合，并扔到 results
+            helper(subset, nums, i + 1, results);
+            // [1,2] -> [1]  回溯
+            subset.remove(subset.size() - 1);
+        }
+        
+        // 3. 递归的出口
+        // return;
+    }
+}
